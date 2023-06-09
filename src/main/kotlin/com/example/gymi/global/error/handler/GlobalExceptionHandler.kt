f8788d75a71ae5f1a16d547bc7ce4c53cdcc0e61
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-09 22:29:39 +0900

    :memo: GlobalExceptionHandler 작성

---
+++ src/main/kotlin/com/example/gymi/global/error/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,33 @@
+package com.example.gymi.global.error.handler
+
+import com.example.gymi.global.error.ErrorCode
+import com.example.gymi.global.error.ErrorResponse
+import com.example.gymi.global.error.exception.BasicException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
+
+    @ExceptionHandler(BasicException::class)
+    fun globalExceptionHandler(request: HttpServletRequest, ex: BasicException): ResponseEntity<ErrorResponse> {
+
+        log.error(ex.errorCode.message)
+
+        log.error(request.requestURI)
+
+        val errorCode: ErrorCode = ex.errorCode
+
+        return ResponseEntity(
+
+                ErrorResponse(status = errorCode.status, message = errorCode.message),
+                HttpStatus.valueOf(errorCode.status)
+        )
+    }
+}
