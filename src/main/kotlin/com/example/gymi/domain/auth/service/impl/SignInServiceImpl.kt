dcc56f88b5d70df963c1eec266e447e257aed47c
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-03 07:55:41 +0900

    :memo: SignInServiceImpl 작성

---
+++ src/main/kotlin/com/example/gymi/domain/auth/service/impl/SignInServiceImpl.kt
@@ -0,0 +1,88 @@
+package com.example.gymi.domain.auth.service.impl
+
+import com.example.gymi.domain.auth.exception.RoleNotExistException
+import com.example.gymi.domain.auth.presentation.data.dto.SignInDto
+import com.example.gymi.domain.auth.presentation.data.response.SignInResponseDto
+import com.example.gymi.domain.auth.service.SignInService
+import com.example.gymi.domain.auth.util.AuthUtil
+import com.example.gymi.domain.user.enums.Role
+import com.example.gymi.domain.user.repository.UserRepository
+import com.example.gymi.global.gauth.properties.GAuthProperties
+import com.example.gymi.global.security.jwt.TokenProvider
+import gauth.GAuth
+import gauth.GAuthToken
+import gauth.GAuthUserInfo
+import org.springframework.stereotype.Service
+import java.time.ZonedDateTime
+
+@Service
+class SignInServiceImpl(
+        private val gAuthProperties: GAuthProperties,
+        private val userRepository: UserRepository,
+        private val tokenProvider: TokenProvider,
+        private val gAuth: GAuth,
+        private val authUtil: AuthUtil,
+) : SignInService {
+
+
+    override fun execute(signInDto: SignInDto): SignInResponseDto {
+        val gAuthToken: GAuthToken = gAuth.generateToken(
+                signInDto.code,
+                gAuthProperties.clientId,
+                gAuthProperties.clientSecret,
+                gAuthProperties.redirectUri
+        )
+        val gAuthUserInfo: GAuthUserInfo = gAuth.getUserInfo(gAuthToken.accessToken)
+        val role = getRoleByGauthInfo(gAuthUserInfo.role, gAuthUserInfo.email)
+        val token = signInDto.token
+
+        val accessToken: String = tokenProvider.generateAccessToken(gAuthUserInfo.email, role)
+        val refreshToken: String = tokenProvider.generateRefreshToken(gAuthUserInfo.email, role)
+        val accessExp: ZonedDateTime = tokenProvider.accessExpiredTime
+        val refreshExp: ZonedDateTime = tokenProvider.refreshExpiredTime
+
+        if(role == Role.ROLE_ADMIN) {
+            createAdminOrRefreshToken(gAuthUserInfo, refreshToken, token)
+        } else {
+            createUserOrRefreshToken(gAuthUserInfo, refreshToken, token)
+        }
+
+        return SignInResponseDto(
+                accessToken = accessToken,
+                refreshToken = refreshToken,
+                accessExp = accessExp,
+                refreshExp = refreshExp
+        )
+    }
+
+    private fun getRoleByGauthInfo(role: String, email: String): Role {
+        val user = userRepository.findByEmail(email) ?:
+        return when (role) {
+            "ROLE_STUDENT" -> Role.ROLE_STUDENT
+            "ROLE_TEACHER" -> Role.ROLE_ADMIN
+            else -> throw RoleNotExistException()
+        }
+        if(user.roles.contains(Role.ROLE_ADMIN))
+            return Role.ROLE_ADMIN
+        return Role.ROLE_STUDENT
+    }
+
+    private fun createUserOrRefreshToken(gAuthUserInfo: GAuthUserInfo, refreshToken: String, token: String?) {
+        val userInfo = userRepository.findByEmail(gAuthUserInfo.email)
+        if (userInfo == null) {
+            authUtil.saveNewUser(gAuthUserInfo, refreshToken, token)
+        } else {
+            authUtil.saveNewRefreshToken(userInfo, refreshToken, token)
+        }
+    }
+
+    private fun createAdminOrRefreshToken(gAuthUserInfo: GAuthUserInfo, refreshToken: String, token: String?) {
+        val adminInfo = userRepository.findByEmail(gAuthUserInfo.email)
+        if (adminInfo == null) {
+            authUtil.saveNewAdmin(gAuthUserInfo, refreshToken, token)
+        } else {
+            authUtil.saveNewRefreshToken(adminInfo, refreshToken, token)
+        }
+    }
+
+}
