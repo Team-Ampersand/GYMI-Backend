5a2c27942ad52ced12a9c7c056291e0f0b077970
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-03 12:00:05 +0900

    :memo: NotBlank 추가

--- src/main/kotlin/com/example/gymi/domain/auth/presentation/data/request/SignInRequestDto.kt
+++ src/main/kotlin/com/example/gymi/domain/auth/presentation/data/request/SignInRequestDto.kt
@@ -1,6 +1,9 @@
 package com.example.gymi.domain.auth.presentation.data.request
 
+import javax.validation.constraints.NotBlank
+
 data class SignInRequestDto(
+        @field:NotBlank
         val code: String,
         val token: String?
 )
