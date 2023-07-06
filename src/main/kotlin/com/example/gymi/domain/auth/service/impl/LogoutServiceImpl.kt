67fbeefb977bf338d39d742473b15a69bcc0d53c
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-06 23:25:16 +0900

    :memo: Transactional 어노테이션 추가

--- src/main/kotlin/com/example/gymi/domain/auth/service/impl/LogoutServiceImpl.kt
+++ src/main/kotlin/com/example/gymi/domain/auth/service/impl/LogoutServiceImpl.kt
@@ -7,8 +7,10 @@ import com.example.gymi.domain.user.entity.User
 import com.example.gymi.domain.user.exception.UserNotFoundException
 import com.example.gymi.global.util.UserUtil
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(rollbackFor = [Exception::class])
 class LogoutServiceImpl(
     private val userUtil: UserUtil,
     private val refreshTokenRepository: RefreshTokenRepository
