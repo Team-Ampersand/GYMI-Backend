75bb4f0fcd98c03fda8ecff5214a5b5128171c44
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-21 17:56:42 +0900

    :memo: NoticeFileRepository 에 findAllByNoticeId 메서드 추가

--- src/main/kotlin/com/example/gymi/domain/notice/repository/NoticeFileRepository.kt
+++ src/main/kotlin/com/example/gymi/domain/notice/repository/NoticeFileRepository.kt
@@ -4,4 +4,6 @@ import com.example.gymi.domain.notice.entity.NoticeFile
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface NoticeFileRepository : JpaRepository<NoticeFile, Long> {
+
+    fun findAllByNoticeId(id: Long): List<NoticeFile>
 }
