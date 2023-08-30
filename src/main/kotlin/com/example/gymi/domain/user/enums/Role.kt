f0d6419dbb46383c637c0e5e9323ca07aefb5c61
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-30 16:39:52 +0900

    :memo: Role 에 ROLE_TEACHER enum 추가

--- src/main/kotlin/com/example/gymi/domain/user/enums/Role.kt
+++ src/main/kotlin/com/example/gymi/domain/user/enums/Role.kt
@@ -3,7 +3,7 @@ package com.example.gymi.domain.user.enums
 import org.springframework.security.core.GrantedAuthority
 
 enum class Role(description: String) : GrantedAuthority {
-    ROLE_STUDENT("학생"), ROLE_ADMIN("관리자");
+    ROLE_STUDENT("학생"), ROLE_ADMIN("관리자"), ROLE_TEACHER("선생님");
 
     override fun getAuthority(): String = name
 }
