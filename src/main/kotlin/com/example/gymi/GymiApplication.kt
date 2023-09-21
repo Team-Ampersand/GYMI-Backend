9545afa898a02238acc18798432dbc865f714833
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-22 08:22:00 +0900

    :memo: GymiApplication 에 프로젝트 시간대 설정 추가

--- src/main/kotlin/com/example/gymi/GymiApplication.kt
+++ src/main/kotlin/com/example/gymi/GymiApplication.kt
@@ -3,11 +3,15 @@ package com.example.gymi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class GymiApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     runApplication<GymiApplication>(*args)
 }
