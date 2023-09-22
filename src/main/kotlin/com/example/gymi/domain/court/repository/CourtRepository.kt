6528fa6481b9bcb422a42161fcd04a3261132730
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-22 18:08:01 +0900

    :memo: CourtRepository 에 lock 옵션이 적용된 findCourtByIdAndCourtNumber 메서드 추가

--- src/main/kotlin/com/example/gymi/domain/court/repository/CourtRepository.kt
+++ src/main/kotlin/com/example/gymi/domain/court/repository/CourtRepository.kt
@@ -2,9 +2,14 @@ package com.example.gymi.domain.court.repository
 
 import com.example.gymi.domain.court.entity.Court
 import com.example.gymi.domain.court.enum.CourtNumber
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.repository.CrudRepository
+import javax.persistence.LockModeType
 
 interface CourtRepository : CrudRepository<Court, Long> {
 
     fun findByCourtNumber(courtNumber: CourtNumber): Court
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    fun findCourtByIdAndCourtNumber(id: Long, courtNumber: CourtNumber): Court
 }
