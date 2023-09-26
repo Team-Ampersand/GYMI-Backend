744737321928922796d09ef9a70908c7c2b2d305
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-26 19:45:19 +0900

    :memo: RequestParam 에서 PathVariable 을 사용하게 수정

--- src/main/kotlin/com/example/gymi/domain/court/presentation/CourtController.kt
+++ src/main/kotlin/com/example/gymi/domain/court/presentation/CourtController.kt
@@ -7,7 +7,7 @@ import com.example.gymi.global.annotation.RequestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,8 +16,8 @@ class CourtController(
     private val getCourtInfoService: GetCourtInfoService
 ) {
 
-    @GetMapping
-    fun getCourtInfo(@RequestParam courtNumber: CourtNumber): ResponseEntity<ReservationCourtInfoResponseDto> =
+    @GetMapping("/{courtNumber}")
+    fun getCourtInfo(@PathVariable courtNumber: CourtNumber): ResponseEntity<ReservationCourtInfoResponseDto> =
         getCourtInfoService.execute(courtNumber)
             .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 }
