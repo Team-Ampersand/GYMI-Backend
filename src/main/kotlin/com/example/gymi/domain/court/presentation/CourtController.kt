0240aa4b5b2930126306c99e1ed7c2f7e588275f
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-26 16:08:02 +0900

    :sparkles: CourtController 작성 및 특정 코트 조회 api 등록

---
+++ src/main/kotlin/com/example/gymi/domain/court/presentation/CourtController.kt
@@ -0,0 +1,23 @@
+package com.example.gymi.domain.court.presentation
+
+import com.example.gymi.domain.court.enum.CourtNumber
+import com.example.gymi.domain.court.presentation.data.response.ReservationCourtInfoResponseDto
+import com.example.gymi.domain.court.service.GetCourtInfoService
+import com.example.gymi.global.annotation.RequestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestController("/court")
+class CourtController(
+    private val getCourtInfoService: GetCourtInfoService
+) {
+
+    @GetMapping
+    fun getCourtInfo(@RequestParam courtNumber: CourtNumber): ResponseEntity<ReservationCourtInfoResponseDto> =
+        getCourtInfoService.execute(courtNumber)
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
+}
