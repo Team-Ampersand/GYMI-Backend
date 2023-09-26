da98f88899e0085e3300f881c3198de4a540b20f
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-26 21:45:57 +0900

    :memo: SecurityConfig 에 전체 코트 조회 api 권한 설정 추가

--- src/main/kotlin/com/example/gymi/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/gymi/global/security/SecurityConfig.kt
@@ -49,6 +49,7 @@ class SecurityConfig(
             .antMatchers(HttpMethod.POST, "/reservation").authenticated()
             .antMatchers(HttpMethod.DELETE, "/reservation").authenticated()
 
+            .antMatchers(HttpMethod.GET, "/court").authenticated()
             .antMatchers(HttpMethod.GET, "/court/{courtNumber}").authenticated()
 
             .anyRequest().denyAll()
