f3b798a093f6e1e7e622612f0fed9c92c062c6c6
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-26 19:44:33 +0900

    :memo: rollbackFor 옵션 추가

--- src/main/kotlin/com/example/gymi/domain/court/service/impl/GetCourtInfoServiceImpl.kt
+++ src/main/kotlin/com/example/gymi/domain/court/service/impl/GetCourtInfoServiceImpl.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = true, rollbackFor = [Exception::class])
 class GetCourtInfoServiceImpl(
     private val findReservationCourtUtil: FindReservationCourtUtil
 ) : GetCourtInfoService {
