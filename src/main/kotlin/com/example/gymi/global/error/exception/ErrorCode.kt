519c3a44ef1f517ab817f8ca61c121355332d7d1
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-05 10:55:09 +0900

    :memo: ErrorCode 에 내가 예약한 코트 반환 api 관련 enum 추가

--- src/main/kotlin/com/example/gymi/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/gymi/global/error/exception/ErrorCode.kt
@@ -28,6 +28,7 @@ enum class ErrorCode(
     COURT_RESERVATION_HOUR_OR_MINUTE_IS_NOT_VALID(401, "체육관 신청은 오전 11시 30분부터 오전 11시 59분까지 혹은 오후 5시 30분부터 오후 5시 59분까지만 가능합니다."),
     COURT_RESERVATION_CANCEL_DAY_IS_NOT_VALID(401, "토요일 혹은 일요일에는 체육관을 신청 취소 할 수 없습니다."),
     COURT_RESERVATION_CANCEL_HOUR_OR_MINUTE_IS_NOT_VALID(401, "체육관 신청 취소는 오전 11시 30분부터 오전 11시 59분까지 혹은 오후 5시 30분부터 오후 5시 59분까지만 가능합니다."),
+    COURT_RESERVATION_NOT_FOUND(404, "코트 예약 내역이 존재하지 않습니다."),
     COURT_RESERVATION_OVER(409, "해당 코트의 최대 인원을 초과했습니다."),
     COURT_RESERVATION_CANT_CANCEL(409, "현재 신청 취소를 할 수 있는 상태가 아닙니다."),
     ALREADY_RESERVATION_COURT_IS_EXISTS(409, "이미 예약한 코트가 존재합니다.")
