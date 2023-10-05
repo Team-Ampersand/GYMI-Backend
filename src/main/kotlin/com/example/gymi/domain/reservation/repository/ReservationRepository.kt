a71f7ec7e3914d4403deaa3395d25d8372b77d51
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-05 10:56:37 +0900

    :memo: ReservationRepository 에 findByUser 메서드 추가

--- src/main/kotlin/com/example/gymi/domain/reservation/repository/ReservationRepository.kt
+++ src/main/kotlin/com/example/gymi/domain/reservation/repository/ReservationRepository.kt
@@ -5,6 +5,7 @@ import com.example.gymi.domain.user.entity.User
 import org.springframework.data.repository.CrudRepository
 
 interface ReservationRepository : CrudRepository<Reservation, Long> {
-
     fun deleteByUser(user: User)
+
+    fun findByUser(user: User): Reservation?
 }
